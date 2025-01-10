22a580aeac3dd0577e069ca715fbc3091261774b
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-01-10 11:17:53 -0300

    Sistema de Inputs

--- Mech-Dungeon/Assets/Scripts/Controlador/ControladorDeInput.cs
+++ Mech-Dungeon/Assets/Scripts/Controlador/ControladorDeInput.cs
@@ -1,9 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ControladorDeInput : MonoBehaviour
 {
+
+    public Vector2 MovementInput;
+    public bool dash;
+#region VerificaçãoDeUnicidade
     public static ControladorDeInput instance;
-    [SerializeField] int sim;
+    void Awake(){
+    if (instance == null){
+        instance = this;
+    }
+    else {
+    Destroy(gameObject);
+    }
+    }
+    #endregion
+#region Inputs
+
+
+    public void OnMove(InputValue valor){
+        MovementInput = valor.Get<Vector2>();
+    }
+    public void OnDash(InputValue valor){
+        dash = valor.isPressed;
+    }
+#endregion
+#region Correcao
+    void LateUpdate(){
+        if (dash){
+            dash = false ;
+        }
+        
+    }
+    
+#endregion
 }
