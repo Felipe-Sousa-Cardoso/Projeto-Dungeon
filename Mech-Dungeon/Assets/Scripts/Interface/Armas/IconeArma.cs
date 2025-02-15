e947d7971d612fc358cfd09da178c45eef7c447d
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-02-15 12:02:54 -0300

    interface das munições

--- Mech-Dungeon/Assets/Scripts/Interface/Armas/IconeArma.cs
+++ Mech-Dungeon/Assets/Scripts/Interface/Armas/IconeArma.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class IconeArma : MonoBehaviour
 {
     [SerializeField] Image arma;
+    [SerializeField] RectTransform munições;
     [SerializeField] Image cobertura;
     Sprite armaAnterior;
     float x; //Usado para controlar o FillAmont da cobertura
     bool coberturaA; //Verifica quando a cobertura dve ser alterada
     [SerializeField] DadosDaArma daArma;
+    int muniçãoAnterior;
     
     
     void Update()
@@ -35,6 +37,13 @@ public class IconeArma : MonoBehaviour
         {
             cobertura.color = new Vector4(0.5f, 0.5f, 0.5f, 0);
             coberturaA = true;
+        }
+        if (muniçãoAnterior != daArma.MuniçãoAtual)
+        {
+            muniçãoAnterior = daArma.MuniçãoAtual;
+            munições.sizeDelta = new Vector2 (13, muniçãoAnterior*8);
+            munições.anchoredPosition = new Vector2 (920, -375+muniçãoAnterior*12);
+
         }
     }
 
